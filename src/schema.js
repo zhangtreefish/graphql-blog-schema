dd23dd0e1a24000f768627fe215d91147af789f3
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-11-17 14:42:44 -0600

    Feat: implement declaring graphQL query

--- src/schema.js
+++ src/schema.js
@@ -1,21 +1,21 @@
 // import * as _ from 'underscore';
 
 // This is the Dataset in our blog
-// import PostsList from './data/posts';
-// import AuthorsList from './data/authors';
+import PostsList from './data/posts';
+import AuthorsList from './data/authors';
 // import {CommentList, ReplyList} from './data/comments';
 
 import {
   // These are the basic GraphQL types
   // GraphQLInt,
-  // GraphQLFloat,
+  GraphQLFloat,
   GraphQLString,
-  // GraphQLList,
+  GraphQLList,
   GraphQLObjectType,
   // GraphQLEnumType,
 
   // This is used to create required fields and arguments
-  // GraphQLNonNull,
+  GraphQLNonNull,
 
   // This is the class we need to create the schema
   GraphQLSchema
@@ -24,12 +24,46 @@ import {
 /**
   DEFINE YOUR TYPES BELOW
 **/
+const Author = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author",
+  fields: () => ({
+    _id: {type: new GraphQLNonNull(GraphQLString)},
+    name: {type: GraphQLString}
+  })
+});
 
+const Post = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => ({
+    _id: {type: new GraphQLNonNull(GraphQLString)},
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: function(post) {
+        return post.title || "not available"
+      }
+    },
+    content: {type: GraphQLString},
+    author: {
+      type: Author,
+      resolve: function(post) {
+        return AuthorsList.find(a => a._id === post.author);
+      }
+    }
+  })
+});
 // This is the Root Query
 const Query = new GraphQLObjectType({
   name: 'BlogSchema',
-  description: 'Root of the Blog Schema',
+  description: "Root of the Blog Schema",
   fields: () => ({
+    posts: {
+      type: new GraphQLList(Post),
+      resolve: function() {
+        return PostsList;
+      }
+    },
     echo: {
       type: GraphQLString,
       description: 'Echo what you enter',
@@ -42,7 +76,6 @@ const Query = new GraphQLObjectType({
     }
   })
 });
-
 // The Schema
 const Schema = new GraphQLSchema({
   query: Query
