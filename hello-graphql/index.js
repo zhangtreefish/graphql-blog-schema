8865aa8482c05e100327824cc31eec09ccd1d223
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-12-12 11:53:15 -0600

    Feat: implement querying in .js using graphql module

---
+++ hello-graphql/index.js
@@ -0,0 +1,57 @@
+import {
+  // These are the basic GraphQL types
+  GraphQLInt,
+  GraphQLFloat,
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLEnumType,
+
+  // This is used to create required fields and arguments
+  GraphQLNonNull,
+
+  // This is the class we need to create the schema
+  GraphQLSchema,
+
+  // This function is used execute GraphQL queries
+  graphql
+} from 'graphql';
+
+const Query = new GraphQLObjectType({
+  name: 'RootQueries',
+  fields: () => ({
+    echo: {
+      type: GraphQLString,
+      args: {
+        message: {type: GraphQLString}
+      },
+      resolve(rootValue, args) {
+        return `received: ${args.message}`;
+      }
+    }
+  })
+});
+
+const Schema = new GraphQLSchema({
+  query: Query
+});
+
+// let query1 = `
+//   {
+//     receivedMessage: echo(message: "Hello")
+//   }
+// `;
+
+// graphql(Schema, query1).then(function(result) {
+//   console.log(result);
+// });
+
+let query = `
+  query getEcho($inputMessage: String) {
+    receivedMessage: echo(message: $inputMessage)
+  }
+`;
+
+graphql(Schema, query, null, null, {inputMessage: "Hello"}).then(function(result) {
+  console.log(result);
+});
